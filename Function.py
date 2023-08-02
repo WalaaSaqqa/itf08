49c06d03c111f568993ab9485293c24d5ecc6704
WalaaSaqqa <walaa.shak.89@gmail.com>
2023-08-02 16:21:00 +0300

    Functions Walaa Saqqa

---
+++ Function.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+def add(num1, num2):
+    return num1 + num2
+def subtract(num1, num2):
+    return num1 - num2
+def multiply(num1, num2):
+    return num1 * num2
+def div(num1, num2):
+    return num1 / num2
+
+print(f"Add:{add(num1, num2)}")
+print(f"Subtraction:{subtract(num1, num2)}")
+print(f"Multiplication:{multiply(num1, num2)}")
+print(f"Division:{div(num1, num2)}")
+
+
+
